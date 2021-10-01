35c62931dad18b1f9b97c3830db06f94504e5568
Ras <Ras@GHOST>
2021-10-01 10:40:46 +0300

    commiting first

---
+++ MVC c sharp tuttorial/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_c_sharp_tuttorial
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MVC c sharp tuttorial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_c_sharp_tuttorial.MvcApplication" Language="C#" %>
